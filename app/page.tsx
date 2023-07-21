9ad88ea00fd1726dba4e196af0e5dd52a4515d57
Devesh Sharma <97972618+Devesh18Sharma@users.noreply.github.com>
2023-07-21 12:06:09 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Home=()=>{
+    return <div>
+        <section className="flex-start flex-col padding mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>LoadMore</h1>
+        </section>
+    </div>
+}
+export default Home;
